d54ff583668af6818b7f24177de59e80f40e59d3
lgykuuga <645241426@qq.com>
2019-11-10 15:26:06 +0800

    yaml

--- jeefast-admin/src/main/java/com/lgy/web/controller/system/SysProfileController.java
+++ jeefast-admin/src/main/java/com/lgy/web/controller/system/SysProfileController.java
@@ -1,16 +1,5 @@
 package com.lgy.web.controller.system;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.multipart.MultipartFile;
 import com.lgy.common.annotation.Log;
 import com.lgy.common.config.Global;
 import com.lgy.common.core.controller.BaseController;
@@ -22,23 +11,29 @@ import com.lgy.framework.shiro.service.SysPasswordService;
 import com.lgy.framework.util.ShiroUtils;
 import com.lgy.system.domain.SysUser;
 import com.lgy.system.service.ISysUserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  * 个人信息 业务处理
- * 
+ *
  * @author lgy
  */
 @Controller
 @RequestMapping("/system/user/profile")
-public class SysProfileController extends BaseController
-{
+public class SysProfileController extends BaseController {
     private static final Logger log = LoggerFactory.getLogger(SysProfileController.class);
 
     private String prefix = "system/user/profile";
 
     @Autowired
     private ISysUserService userService;
-    
+
     @Autowired
     private SysPasswordService passwordService;
 
@@ -46,8 +41,7 @@ public class SysProfileController extends BaseController
      * 个人信息
      */
     @GetMapping()
-    public String profile(ModelMap mmap)
-    {
+    public String profile(ModelMap mmap) {
         SysUser user = ShiroUtils.getSysUser();
         mmap.put("user", user);
         mmap.put("roleGroup", userService.selectUserRoleGroup(user.getUserId()));
@@ -57,19 +51,16 @@ public class SysProfileController extends BaseController
 
     @GetMapping("/checkPassword")
     @ResponseBody
-    public boolean checkPassword(String password)
-    {
+    public boolean checkPassword(String password) {
         SysUser user = ShiroUtils.getSysUser();
-        if (passwordService.matches(user, password))
-        {
+        if (passwordService.matches(user, password)) {
             return true;
         }
         return false;
     }
 
     @GetMapping("/resetPwd")
-    public String resetPwd(ModelMap mmap)
-    {
+    public String resetPwd(ModelMap mmap) {
         SysUser user = ShiroUtils.getSysUser();
         mmap.put("user", userService.selectUserById(user.getUserId()));
         return prefix + "/resetPwd";
@@ -78,22 +69,17 @@ public class SysProfileController extends BaseController
     @Log(title = "重置密码", businessType = BusinessType.UPDATE)
     @PostMapping("/resetPwd")
     @ResponseBody
-    public AjaxResult resetPwd(String oldPassword, String newPassword)
-    {
+    public AjaxResult resetPwd(String oldPassword, String newPassword) {
         SysUser user = ShiroUtils.getSysUser();
-        if (StringUtils.isNotEmpty(newPassword) && passwordService.matches(user, oldPassword))
-        {
+        if (StringUtils.isNotEmpty(newPassword) && passwordService.matches(user, oldPassword)) {
             user.setSalt(ShiroUtils.randomSalt());
             user.setPassword(passwordService.encryptPassword(user.getLoginName(), newPassword, user.getSalt()));
-            if (userService.resetUserPwd(user) > 0)
-            {
+            if (userService.resetUserPwd(user) > 0) {
                 ShiroUtils.setSysUser(userService.selectUserById(user.getUserId()));
                 return success();
             }
             return error();
-        }
-        else
-        {
+        } else {
             return error("修改密码失败，旧密码错误");
         }
     }
@@ -102,8 +88,7 @@ public class SysProfileController extends BaseController
      * 修改用户
      */
     @GetMapping("/edit")
-    public String edit(ModelMap mmap)
-    {
+    public String edit(ModelMap mmap) {
         SysUser user = ShiroUtils.getSysUser();
         mmap.put("user", userService.selectUserById(user.getUserId()));
         return prefix + "/edit";
@@ -113,8 +98,7 @@ public class SysProfileController extends BaseController
      * 修改头像
      */
     @GetMapping("/avatar")
-    public String avatar(ModelMap mmap)
-    {
+    public String avatar(ModelMap mmap) {
         SysUser user = ShiroUtils.getSysUser();
         mmap.put("user", userService.selectUserById(user.getUserId()));
         return prefix + "/avatar";
@@ -126,15 +110,13 @@ public class SysProfileController extends BaseController
     @Log(title = "个人信息", businessType = BusinessType.UPDATE)
     @PostMapping("/update")
     @ResponseBody
-    public AjaxResult update(SysUser user)
-    {
+    public AjaxResult update(SysUser user) {
         SysUser currentUser = ShiroUtils.getSysUser();
         currentUser.setUserName(user.getUserName());
         currentUser.setEmail(user.getEmail());
         currentUser.setPhonenumber(user.getPhonenumber());
         currentUser.setSex(user.getSex());
-        if (userService.updateUserInfo(currentUser) > 0)
-        {
+        if (userService.updateUserInfo(currentUser) > 0) {
             ShiroUtils.setSysUser(userService.selectUserById(currentUser.getUserId()));
             return success();
         }
@@ -147,25 +129,19 @@ public class SysProfileController extends BaseController
     @Log(title = "个人信息", businessType = BusinessType.UPDATE)
     @PostMapping("/updateAvatar")
     @ResponseBody
-    public AjaxResult updateAvatar(@RequestParam("avatarfile") MultipartFile file)
-    {
+    public AjaxResult updateAvatar(@RequestParam("avatarfile") MultipartFile file) {
         SysUser currentUser = ShiroUtils.getSysUser();
-        try
-        {
-            if (!file.isEmpty())
-            {
+        try {
+            if (!file.isEmpty()) {
                 String avatar = FileUploadUtils.upload(Global.getAvatarPath(), file);
                 currentUser.setAvatar(avatar);
-                if (userService.updateUserInfo(currentUser) > 0)
-                {
+                if (userService.updateUserInfo(currentUser) > 0) {
                     ShiroUtils.setSysUser(userService.selectUserById(currentUser.getUserId()));
                     return success();
                 }
             }
             return error();
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             log.error("修改头像失败！", e);
             return error(e.getMessage());
         }
