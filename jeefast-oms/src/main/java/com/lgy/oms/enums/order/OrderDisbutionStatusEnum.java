f44b53091e63b8834f053bc37c95e573c93e659c
werwrer <7624572@qq.com>
2019-12-21 14:18:59 +0800

    add distibution status enum

---
+++ jeefast-oms/src/main/java/com/lgy/oms/enums/order/OrderDisbutionStatusEnum.java
@@ -0,0 +1,69 @@
+package com.lgy.oms.enums.order;
+
+import com.lgy.system.domain.vo.Config;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 配货单状态
+ * @author wh
+ * @since 2019-12-21
+ */
+public enum OrderDisbutionStatusEnum {
+    /**
+     * 等待配货
+     */
+    WAITDISBUTION(0, "等待配货"),
+    /**
+     * 取消
+     */
+    CANCLE(1, "取消"),
+    /**
+     * 已配货
+     */
+    DISTIBUTION(2, "已配货"),
+    /**
+     * 待发货
+     */
+    WAITDILVER(3, "待发货"),
+    /**
+     * 已发货
+     */
+    DILIVERGOODS(5, "已发货");
+
+    private Integer value;
+    private String name;
+
+    private static List<Config> list = new ArrayList<>();
+
+    OrderDisbutionStatusEnum(Integer value, String name) {
+        this.value = value;
+        this.name = name;
+    }
+
+    public Integer getValue() {
+        return value;
+    }
+
+    public void setValue(Integer value) {
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public static List<Config> getList(){
+        if(list.isEmpty()){
+            for (OrderDisbutionStatusEnum temp : OrderDisbutionStatusEnum.values()) {
+                list.add(new Config(temp.value.toString(), temp.getName()));
+            }
+        }
+        return list;
+    }
+}
