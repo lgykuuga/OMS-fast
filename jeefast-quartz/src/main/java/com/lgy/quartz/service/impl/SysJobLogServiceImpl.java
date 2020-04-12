b0f5a41bea8b2f5964e1049f0fc63df36bb3bbf0
lgykuuga <645241426@qq.com>
2020-04-12 21:32:19 +0800

    2.1

--- jeefast-quartz/src/main/java/com/lgy/quartz/service/impl/SysJobLogServiceImpl.java
+++ jeefast-quartz/src/main/java/com/lgy/quartz/service/impl/SysJobLogServiceImpl.java
@@ -1,80 +1,74 @@
 package com.lgy.quartz.service.impl;
 
-import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
+import com.lgy.common.core.text.Convert;
 import com.lgy.quartz.domain.SysJobLog;
 import com.lgy.quartz.mapper.SysJobLogMapper;
 import com.lgy.quartz.service.ISysJobLogService;
-import com.lgy.common.core.text.Convert;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * 定时任务调度日志信息 服务层
- * 
+ *
  * @author lgy
  */
 @Service
-public class SysJobLogServiceImpl implements ISysJobLogService
-{
+public class SysJobLogServiceImpl implements ISysJobLogService {
     @Autowired
     private SysJobLogMapper jobLogMapper;
 
     /**
      * 获取quartz调度器日志的计划任务
-     * 
+     *
      * @param jobLog 调度日志信息
      * @return 调度任务日志集合
      */
     @Override
-    public List<SysJobLog> selectJobLogList(SysJobLog jobLog)
-    {
+    public List<SysJobLog> selectJobLogList(SysJobLog jobLog) {
         return jobLogMapper.selectJobLogList(jobLog);
     }
 
     /**
      * 通过调度任务日志ID查询调度信息
-     * 
-     * @param jobId 调度任务日志ID
+     *
+     * @param jobLogId 调度任务日志ID
      * @return 调度任务日志对象信息
      */
     @Override
-    public SysJobLog selectJobLogById(Long jobLogId)
-    {
+    public SysJobLog selectJobLogById(Long jobLogId) {
         return jobLogMapper.selectJobLogById(jobLogId);
     }
 
     /**
      * 新增任务日志
-     * 
+     *
      * @param jobLog 调度日志信息
      */
     @Override
-    public void addJobLog(SysJobLog jobLog)
-    {
+    public void addJobLog(SysJobLog jobLog) {
         jobLogMapper.insertJobLog(jobLog);
     }
 
     /**
      * 批量删除调度日志信息
-     * 
+     *
      * @param ids 需要删除的数据ID
      * @return 结果
      */
     @Override
-    public int deleteJobLogByIds(String ids)
-    {
+    public int deleteJobLogByIds(String ids) {
         return jobLogMapper.deleteJobLogByIds(Convert.toStrArray(ids));
     }
 
     /**
      * 删除任务日志
-     * 
+     *
      * @param jobId 调度日志ID
      */
     @Override
-    public int deleteJobLogById(Long jobId)
-    {
+    public int deleteJobLogById(Long jobId) {
         return jobLogMapper.deleteJobLogById(jobId);
     }
 
@@ -82,8 +76,7 @@ public class SysJobLogServiceImpl implements ISysJobLogService
      * 清空任务日志
      */
     @Override
-    public void cleanJobLog()
-    {
+    public void cleanJobLog() {
         jobLogMapper.cleanJobLog();
     }
 }
