7d3986da763afe5b73fc2701307e9c194a9785aa
lgykuuga <645241426@qq.com>
2020-07-03 11:15:46 +0800

    remove

--- jeefast-admin/src/main/java/com/lgy/web/controller/oms/test/TestParamsController.java
+++ jeefast-admin/src/main/java/com/lgy/web/controller/oms/test/TestParamsController.java
@@ -3,12 +3,18 @@ package com.lgy.web.controller.oms.test;
 import com.lgy.common.config.SystemConfig;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang3.ArrayUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * @Description TestController
@@ -41,4 +47,38 @@ public class TestParamsController {
         log.info(sb.toString());
         return sb.toString();
     }
+
+    @PostMapping("/testList")
+    @ApiOperation(value = "对比数据", httpMethod = "POST")
+    public String testList(String list1, String list2) {
+
+        if (StringUtils.isEmpty(list1)) {
+            return "输入单号为空";
+        }
+
+        List<String> orderIdList1 = parse(list1);
+        List<String> orderIdList2 = parse(list2);
+
+        String s = compareList(orderIdList1, orderIdList2);
+        return s.replace(" ", "");
+    }
+
+    /**
+     * 解析
+     *
+     * @param orderIds 订单编号,多条用逗号分隔
+     * @return
+     */
+    private List<String> parse(String orderIds) {
+        String[] orderIdz = orderIds.split(",");
+        return Arrays.asList(orderIdz);
+    }
+
+    /**
+     * 获取集合差集
+     */
+    private static String compareList(List<String> ofbizOrderIdList, List<String> omsOrderIdList) {
+        return ArrayUtils.toString(CollectionUtils.subtract(ofbizOrderIdList, omsOrderIdList));
+    }
+
 }
--- jeefast-oms/src/main/java/com/lgy/oms/interfaces/ods/contant/ODSConstants.java
+++ jeefast-oms/src/main/java/com/lgy/oms/interfaces/ods/contant/ODSConstants.java
@@ -15,14 +15,5 @@ public class ODSConstants {
      * 返回失败标识
      */
     public static final String FAILURE = "failure";
-    /**
-     * 获取订单列表方法接口
-     */
-    public static final String ORDERS_METHOD = "gwall.ods.orders.sold.get";
-
-    /**
-     * 获取单笔订单详情方法接口
-     */
-    public static final String ORDER_FULLINFO_METHOD = "gwall.ods.order.fullinfo.get";
 
 }
